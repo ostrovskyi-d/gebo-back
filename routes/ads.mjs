09cedbee8217c334b13a52dbc9c770295813285a
Denys Ostrovskyi <d.ostrovskyi@pnn.in.ua>
2021-06-28 20:07:38 +0300

    pre-dev: try to deploy

--- routes/ads.mjs
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {
-    Ad
-} = require('../models/index.mjs');
-const {ads} = require('./common.mjs');
-
-router.get('/get-ads', (req, res) => ads(req, res));
-
--- routes/auth.mjs
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/AdsController.mjs');
-
-router.get('/login', controller.login);
-router.post('/register', controller.register);
