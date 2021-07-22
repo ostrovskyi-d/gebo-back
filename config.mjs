c8b90b74f80e09e26db35538d3c394cd47c25450
Denys Ostrovskyi <d.ostrovskyi@pnn.in.ua>
2021-07-23 00:13:48 +0300

    dev: authorization with jwt added

--- config.mjs
+++ config.mjs
@@ -9,7 +9,8 @@ dotenv.config({path: root('../.env')});
 const config = {
     PORT: process.env.PORT || 3001,
     MONGODB_URI: process.env.MONGODB_URI,
-    PER_PAGE: process.env.PER_PAGE || 1
+    PER_PAGE: process.env.PER_PAGE || 1,
+    SESSION_SECRET: process.env.SESSION_SECRET || '123jcaiedif7e8yhofbqeo87'
 };
 
 export default config;
---
+++ services/authService.mjs
@@ -0,0 +1,31 @@
+import expressJwt from 'express-jwt';
+import config from '../config.mjs';
+import UserController from '../controllers/userController.mjs'
+
+
+const {SESSION_SECRET} = config;
+const User = new UserController();
+
+const jwt = () => {
+    const secret = SESSION_SECRET;
+    return expressJwt({ secret, algorithms: ['HS256'], isRevoked }).unless({
+        path: [
+            // public routes that don't require authentication
+            '/add-new-user',
+            '/users-clear'
+        ]
+    });
+}
+
+async function isRevoked(req, payload, done) {
+    const user = await User.getById(payload.sub);
+
+    // revoke token if user no longer exists
+    if (!user) {
+        return done(null, true);
+    }
+
+    done();
+}
+
+export default jwt;
